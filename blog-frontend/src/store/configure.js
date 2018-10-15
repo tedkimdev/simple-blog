73622aa2d2f87144735e3a45a08f3e770d86c082
tedtaedongkim <yenafirst91@gmail.com>
2018-10-14 20:46:40 -0400

    Add pages & route

--- blog-frontend/src/store/configure.js
+++ blog-frontend/src/store/configure.js
@@ -8,6 +8,7 @@ const middlewares = [penderMiddleware()];
 // dev mode. apply Redux Devtools
 const isDev = process.env.NODE_ENV === 'development';
 const devtools = isDev && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+
 const composeEnhancers = devtools || compose;
 
 const configure = (preloadedState) => createStore(reducers, preloadedState,
